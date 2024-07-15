1d96d308f784b1806747b1e501b7400fe087eff4
Ido Shenbach <idoshen1@gmail.com>
2024-07-15 18:22:16 +0300

    wordle class added

--- src/wordle.py
+++ src/wordle.py
@@ -1,10 +1,19 @@
-def loadWords():
+import random
+
+def load_words():
     with open("../data/allowed_words.txt") as f:
         words = f.read().splitlines()
     return words 
 
 def main():
-    print(loadWords())
+    new_game = Wordle()
+
+
+class Wordle:
+    def __init__(self):
+        self.word_bank = load_words()
+        self.target_word = random.choice(self.word_bank)
+        print(self.target_word)
 
 if __name__ == "__main__":
     main()
