6d52ee23a6f0a7349b4058fdf3716d75c9d9ef8f
FatranShahar <faharshatran@gmail.com>
2024-07-15 18:26:51 +0300

    added input validation

--- src/wordle.py
+++ src/wordle.py
@@ -8,12 +8,19 @@ def load_words():
 def main():
     new_game = Wordle()
 
-
 class Wordle:
     def __init__(self):
         self.word_bank = load_words()
         self.target_word = random.choice(self.word_bank)
         print(self.target_word)
+
+    def input_validator():
+        user_input = ''
+        while (len(user_input) != 5 or not user_input.isalpha()):
+            print('insert 5 letter word:')
+            user_input = input()
+
+        return user_input
 
 if __name__ == "__main__":
     main()
